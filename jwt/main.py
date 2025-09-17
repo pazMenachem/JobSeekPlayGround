c9d4facd7a7926ed71e0064d4aae16022bcb711d
Paz_Menachem <paz.menach@gmail.com>
2025-09-17 13:04:02 +0300

    Create infastructures

---
+++ jwt/main.py
@@ -0,0 +1,17 @@
+"""
+Simple JWT Learning Project
+
+This project demonstrates JWT (JSON Web Token) concepts step by step.
+Perfect for learning authentication fundamentals.
+"""
+
+def main():
+    """Main function to run the JWT learning examples."""
+    print("Welcome to JWT Learning Project!")
+    print("This project will teach you JWT concepts step by step.")
+    
+    # We'll add JWT functionality in the next steps
+    print("Ready to learn JWT!")
+
+if __name__ == "__main__":
+    main()
